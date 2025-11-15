94bf73d9aa4e393da71e2a2a9f5794bbeb520f30
Mark Devenyi <markdevenyidev@gmail.com>
2025-11-15 03:46:33 -0500

    feat(embedded): add basic user input handling

--- embedded/comhub.cpp
+++ embedded/comhub.cpp
@@ -2,17 +2,73 @@
 #include "comhub.h"
 #include "main.h"
 
+static char input_buf[NS_COMHUB_BUF_SIZE];
+static comhub_state_t currstate = COMHUB_MENU;
+static int available = 0;
+static const arduino::__FlashStringHelper* strheader = F("[ ======================== ]");
+static const arduino::__FlashStringHelper* strempty = F("[ >                        ]");
+
 void init_comhub(void) {
     Serial.begin(COMHUB_BAUD_RATE, SERIAL_8N1);
     while (!Serial) {}
 
     Serial.println(F(" "));
-    Serial.println(F("[ ======================== ]"));
+    Serial.println(strheader);
     Serial.println(F("[ > Init Nano-Sniffer      ]"));
     Serial.println(F("[ >              v" NS_VERSION_STR " ]"));
-    Serial.println(F("[ >                        ]"));
+    Serial.println(strempty);
     Serial.println(F("[ > Built on:              ]"));
     Serial.println(F("[ >   "__DATE__ " " __TIME__" ]"));
-    Serial.println(F("[ ======================== ]"));
+    Serial.println(strheader);
     Serial.println();
+}
+
+static void show_menu(void) {
+    Serial.println(F("[ =[ Menu ]=============== ]"));
+    Serial.println(strempty);
+    Serial.println(F("[ > [h]elp - show this     ]"));
+    Serial.println(F("[ >                        ]"));
+    Serial.println(strheader);
+}
+
+void handle_comhub(void) {
+    available = Serial.available();
+    if (available > 0) {
+        switch (currstate) {
+            case COMHUB_MENU:
+                handle_menu();
+                break;
+            default:
+                handle_menu();
+                break;
+        }
+
+        /* Drain Serial buffer manually */
+        while (available--) {
+            Serial.read();
+        }
+    }
+}
+
+static inline void handle_menu() {
+    if (available == 1) {
+        char c = Serial.read();
+        if (c == 'h') {
+            show_menu();
+        }
+    } else if (available == 4) {
+        char buf[4] = { 0 };
+        read2Buffer(buf, available);
+        if (strncmp(buf, "help", 4) == 0) {
+            show_menu();
+        }
+    } else {
+        Serial.println("Invalid menu command!");
+    }
+}
+
+static inline void read2Buffer(char* buffer, size_t length) {
+    for (int i = 0; i < length; i++) {
+        buffer[i] = Serial.read();
+    }
 }
--- embedded/comhub.h
+++ embedded/comhub.h
@@ -1,8 +1,19 @@
 #ifndef NS_COMHUB_H
 #define NS_COMHUB_H
 
+#include <stdint.h>
+
 #define COMHUB_BAUD_RATE 115200
+#define NS_COMHUB_BUF_SIZE 256
+
+typedef enum {
+    COMHUB_MENU,
+} comhub_state_t;
 
 void init_comhub(void);
+void handle_comhub(void);
+
+static inline void read2Buffer(char* buffer, size_t length);
+static inline void handle_menu();
 
 #endif // NS_COMHUB_H
