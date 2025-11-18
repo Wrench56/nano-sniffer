4283810d06b1c46195e8de5a700d84d735c06c30
Mark Devenyi <markdevenyidev@gmail.com>
2025-11-18 05:54:17 -0500

    feat(embedded): add `ITransport` class

---
+++ embedded/transport.h
@@ -0,0 +1,19 @@
+#ifndef NS_TRANSPORT_H
+#define NS_TRANSPORT_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+class ITransport {
+public:
+    virtual int available() = 0;
+    virtual int read() = 0;
+    virtual uint32_t write(const uint8_t* data, size_t len) = 0;
+    virtual void flush() {}
+    virtual void drain() {}
+    virtual bool connected() {
+        return true;
+    }
+};
+
+#endif // NS_TRANSPORT_H
